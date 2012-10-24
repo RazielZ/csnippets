79845492f387d94fc7af8b490c075ef0e0e4cbd8
otfallen <f.fallen45@gmail.com>
2012-10-24 01:54:56 +0000

    Fix duplicate entries in config.h file, also free config defs

--- src/config.h
+++ src/config.h
@@ -23,7 +23,7 @@ struct centry_t {
 } while (0)
 
 #define __config_get_val(configentry, key, out, out_len) do { \
-    typeof(configentry) __tmp = (configobj)->def; \
+    typeof(configentry) __tmp = (co)figobj)->def; \
     for (; __tmp != NULL; __tmp = __tmp->next_def) {\
         if (!memcmp(__tmp->key, key, sizeof(key))) { \
             strncpy(out, __tmp->value, out_len); \
@@ -33,58 +33,21 @@ struct centry_t {
     __out[out_len] = '\0'; \
 } while (0)
 
-#define __config_free(configobj) do { \
-    typeof(configobj) __tmp, __p = (configobj);  \
-    while (__p) { \
-        __tmp = __p->next;  \
-        free(__p);          \
-        __p = __tmp;         \
-    }  \
-} while(0)
-
-extern struct centry_t *config_parse(const char *filename);
-
-#endif   /* __config_h */
-
-#ifndef __config_h
-#define __config_h
-
-struct cdef_t {
-    char key[33];
-    char *value;
-    struct cdef_t *next_def;
-};
-
-struct centry_t {
-    char section[32];
-
-    struct cdef_t  *def;
-    struct centry_t *next_entry;
-};
-
-#define __config_get_entry(configobj, section, out) do {\
-    typeof(configob) __tmp = (configobj);    \
-    for (; __tmp != NULL; __tmp = __tmp->next_entry) \
-        if (!memcmp(__tmp->section, section, sizeof(section)))  \
-            out = __tmp; \
-    out = NULL; \
-} while (0)
-
-#define __config_get_val(configentry, key, out, out_len) do { \
-    typeof(configentry) __tmp = (configobj)->def; \
-    for (; __tmp != NULL; __tmp = __tmp->next_def) {\
-        if (!memcmp(__tmp->key, key, sizeof(key))) { \
-            strncpy(out, __tmp->value, out_len); \
-            break; \
-        } \
+#define __config_free_defs(configdef) do { \
+    typeof(configdef) __tmpdef, __pdef = (configdef); \
+    while (__pdef) { \
+        __tmpdef = __p->next; \
+        free(__pdef->value); \
+        free(__pdef); \
+        __pdef = __tmpdef; \
     } \
-    __out[out_len] = '\0'; \
 } while (0)
 
 #define __config_free(configobj) do { \
     typeof(configobj) __tmp, __p = (configobj);  \
     while (__p) { \
         __tmp = __p->next;  \
+        __config_free_defs(__p->def); \
         free(__p);          \
         __p = __tmp;         \
     }  \
